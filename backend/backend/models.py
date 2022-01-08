6953d5145c569b92d7a0b91fea21f3ff07d174ff
kavania2002 <kavan155gondalia@gmail.com>
2022-01-08 14:11:41 +0530

    feat: music uploads directly to folder named as title

--- backend/backend/models.py
+++ backend/backend/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+def upload_path(instance, filename):
+    return str("{}/{}".format(instance, filename))
+    
 class Podcast (models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
+    audio_file = models.FileField(upload_to=upload_path,blank=True, null=True)
 
     def __str__(self):
         return self.title

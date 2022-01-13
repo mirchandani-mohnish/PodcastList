052956573f5bc3ed794e60fa43f86094526c09e5
mirchandani-mohnish <mirchandani.mohnish03@gmail.com>
2022-01-13 20:32:57 +0530

    Transcript Frontend Added - Bootstrap jumbotron used for transcript display. - Transcript is displayed in place of list of podcasts

--- frontend/src/components/TranscriptPopUp.jsx
+++ frontend/src/components/TranscriptPopUp.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 
 class TranscriptPopUp extends Component {
     constructor(props){
@@ -9,6 +12,15 @@ class TranscriptPopUp extends Component {
     render() {
         return (
             <div>
+                 <div className="jumbotron">
+                    <h1 className="display-4">Title</h1>
+                    
+                    <hr className="my-4" />
+                    <p>{this.props.transcript}</p>
+                    <p className="lead">
+                    
+                    </p>
+                </div>
                <a href="" className="btn btn-success" onClick={(e) => {e.preventDefault();this.props.closeButton();}}>Close</a>
             </div>
         );
